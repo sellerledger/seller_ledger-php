cb8943c1b9e8210b27f88668cab558519ee494dc
Corin Schedler <corin@dubdromic.com>
2024-07-08 13:14:38 -0600

    Version bump

--- src/SellerLedger.php
+++ src/SellerLedger.php
@@ -3,7 +3,7 @@ namespace SellerLedger;
 
 class SellerLedger
 {
-  const VERSION = '1.0.0';
+  const VERSION = '1.0.2';
   const DEFAULT_API_URL = 'https://app.sellerledger.com/v1';
 
   protected $client;
